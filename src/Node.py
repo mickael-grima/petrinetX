848075844c42daf19c18fbbbe7e4928f7825882a
mickael <mickael@cliqz.com>
2016-07-24 23:32:50 +0200

    refactoring

--- src/Node.py
+++ src/Node.py
@@ -24,20 +24,3 @@ class Node:
 
     def __repr__(self):
         return '<%s : %s>' % (self.__class__.__name__, self.name)
-
-
-class TimeNode(Node):
-    """ This class represents a node with time in the :class:`TimedPetriNet <petrinet_simulator.TimedPetriNet>`
-        It is the class parent of both classes :class:`TimePlace <petrinet_simulator.TimePlace>`
-        and :class:`TimeTransition <petrinet_simulator.TimeTransition>`
-        It herits from the parent class :class:`Node <petrinet_simulator.Node>`
-    """
-    def __init__(self, name='', logger=logging, time=0.0):
-        super(TimeNode, self).__init__(name=name, logger=logger)
-        if(time >= 0.0):
-            self_time = time
-        else:
-            self_time = 0.0
-        self.time = self_time
-        """Default duration on the node
-        """
